e02042bb7fad2560350dabadb81415fb47fb82be
taoyeqi <taoyeqi@hyperchain.cn>
2018-07-13 20:15:07 +0800

    add https util(include jar error)

---
+++ hammer-utils/src/main/java/com/tao/hammer/utils/HttpsUtils.java
@@ -0,0 +1,277 @@
+package com.tao.hammer.utils;
+
+/**
+ * @author taoyeqi
+ * @version 1.0, 2018/6/12
+ */
+
+import sun.security.x509.X500Name;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import javax.xml.bind.DatatypeConverter;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpsUtils {
+    public static class SSLParams {
+        public SSLSocketFactory sSLSocketFactory;
+        public X509TrustManager trustManager;
+    }
+
+    public static SSLParams getSslSocketFactory(String certificates, String tlspeerCert, String tlspeerPriv, String password) {
+        SSLParams sslParams = new SSLParams();
+        InputStream isCa = null;
+        try {
+            if (Utils.isAbsolutePath(certificates)) {
+                isCa = new FileInputStream(certificates);
+            } else {
+                isCa = HttpsUtils.class.getClassLoader().getResourceAsStream(certificates);
+            }
+            TrustManager[] trustManagers = prepareTrustManager(isCa);
+            KeyManager[] keyManagers = prepareKeyManager(tlspeerCert, tlspeerPriv, password);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            X509TrustManager trustManager = null;
+            if (trustManagers != null) {
+                trustManager = new MyTrustManager(chooseTrustManager(trustManagers));
+            } else {
+                trustManager = new UnSafeTrustManager();
+            }
+            sslContext.init(keyManagers, new TrustManager[]{trustManager}, null);
+            sslParams.sSLSocketFactory = sslContext.getSocketFactory();
+            sslParams.trustManager = trustManager;
+            return sslParams;
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError(e);
+        } catch (KeyManagementException e) {
+            throw new AssertionError(e);
+        } catch (KeyStoreException e) {
+            throw new AssertionError(e);
+        } catch (FileNotFoundException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static HostnameVerifier hyperchainVerifier() {
+        return new HostnameVerifier() {
+            @Override
+            public boolean verify(String s, SSLSession sslSession) {
+                try {
+                    X500Name x500Name = (X500Name) sslSession.getPeerCertificateChain()[0].getIssuerDN();
+                    String commonName = x500Name.getCommonName();
+                    return commonName.equals("hyperchain.cn");
+                } catch (SSLPeerUnverifiedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        };
+    }
+
+    private class UnSafeHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+
+    private static class UnSafeTrustManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[]{};
+        }
+    }
+
+    private static TrustManager[] prepareTrustManager(InputStream... certificates) {
+        if (certificates == null || certificates.length <= 0) return null;
+        try {
+
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null);
+            int index = 0;
+            for (InputStream certificate : certificates) {
+                String certificateAlias = Integer.toString(index++);
+                keyStore.setCertificateEntry(certificateAlias, certificateFactory.generateCertificate(certificate));
+                try {
+                    if (certificate != null)
+                        certificate.close();
+                } catch (IOException e) {
+                }
+            }
+            TrustManagerFactory trustManagerFactory = null;
+
+            trustManagerFactory = TrustManagerFactory.
+                    getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+
+            return trustManagerFactory.getTrustManagers();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    private static KeyManager[] prepareKeyManager(String tlspeerCert, String tlspeerPriv, String password) {
+        try {
+//            KeyStore clientKeyStore = KeyStore.getInstance("JKS");
+//            clientKeyStore.load(bksFile, password.toCharArray());
+            KeyStore clientKeyStore = createKeyStore(tlspeerCert, tlspeerPriv, password);
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(clientKeyStore, password.toCharArray());
+            return keyManagerFactory.getKeyManagers();
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static X509TrustManager chooseTrustManager(TrustManager[] trustManagers) {
+        for (TrustManager trustManager : trustManagers) {
+            if (trustManager instanceof X509TrustManager) {
+                return (X509TrustManager) trustManager;
+            }
+        }
+        return null;
+    }
+
+    private static class MyTrustManager implements X509TrustManager {
+        private X509TrustManager defaultTrustManager;
+        private X509TrustManager localTrustManager;
+
+        public MyTrustManager(X509TrustManager localTrustManager) throws NoSuchAlgorithmException, KeyStoreException {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            defaultTrustManager = chooseTrustManager(trustManagerFactory.getTrustManagers());
+            this.localTrustManager = localTrustManager;
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            try {
+                defaultTrustManager.checkServerTrusted(chain, authType);
+            } catch (CertificateException ce) {
+                localTrustManager.checkServerTrusted(chain, authType);
+            }
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+    /**
+     * Create a KeyStore from standard PEM files
+     *
+     * @param privateKeyPem the private key PEM file
+     * @param certificatePem the certificate(s) PEM file
+     * @param password to set to protect the private key
+     */
+    public static KeyStore createKeyStore(String certificatePem, String privateKeyPem, final String password) throws Exception {
+        final X509Certificate[] cert = createCertificates(certificatePem);
+        final KeyStore keystore = KeyStore.getInstance("JKS");
+        keystore.load(null);
+        // Import private key
+        final PrivateKey key = CertUtils.getPrivateKeyFromPEM(privateKeyPem);
+        keystore.setKeyEntry("tlsCertPriv", key, password.toCharArray(), cert);
+        return keystore;
+    }
+
+    private static X509Certificate[] createCertificates(String certificatePem) throws Exception {
+        File tlsCert = null;
+        if (Utils.isAbsolutePath(certificatePem)) {
+            tlsCert = new File(certificatePem);
+        } else {
+            tlsCert = new File(HttpsUtils.class.getClassLoader().getResource(certificatePem).toURI());
+        }
+        final List<X509Certificate> result = new ArrayList<X509Certificate>();
+        final BufferedReader r = new BufferedReader(new FileReader(tlsCert));
+        String s = r.readLine();
+        if (s == null || !s.contains("BEGIN CERTIFICATE")) {
+            r.close();
+            throw new IllegalArgumentException("No CERTIFICATE found");
+        }
+        StringBuilder b = new StringBuilder();
+        while (s != null) {
+            if (s.contains("END CERTIFICATE")) {
+                String hexString = b.toString();
+                final byte[] bytes = DatatypeConverter.parseBase64Binary(hexString);
+                X509Certificate cert = generateCertificateFromPEM(bytes);
+                result.add(cert);
+                b = new StringBuilder();
+            } else {
+                if (!s.startsWith("----")) {
+                    b.append(s);
+                }
+            }
+            s = r.readLine();
+        }
+        r.close();
+
+        return result.toArray(new X509Certificate[result.size()]);
+    }
+
+    private static X509Certificate generateCertificateFromPEM(byte[] certBytes) throws CertificateException {
+        final CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(certBytes));
+    }
+}
